4c19e753a6b29ac32c5ac3c7c8d71db663b64285
Julien <jjy.quere@gmail.com>
2025-07-17 13:12:38 +0200

    Wip

--- config/plugins.js
+++ config/plugins.js
@@ -1,4 +1,30 @@
 module.exports = () => ({
+  translate: {
+    enabled: true,
+    config: {
+      // Add the name of your provider here (for example 'deepl' for strapi-provider-translate-deepl or the full package name)
+      provider: "deepl",
+      providerOptions: {
+        apiKey: process.env.DEEPL_API_KEY,
+      },
+      localeMap: {
+        EN: "EN-US",
+      },
+      // Which field types are translated (default string, text, richtext, components and dynamiczones)
+      // Either string or object with type and format
+      // Possible formats: plain, markdown, html, jsonb (default plain)
+      translatedFieldTypes: [
+        "string",
+        { type: "blocks", format: "jsonb" },
+        { type: "text", format: "plain" },
+        { type: "richtext", format: "markdown" },
+        "component",
+        "dynamiczone",
+      ],
+      // If relations should be translated (default true)
+      translateRelations: true,
+    },
+  },
   documentation: {
     enabled: true,
     config: {
